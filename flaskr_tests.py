9c1c536e52258fe86a12017e8a01e590f8280cc8
Hirat Rahi <hrahi@iwu.edu>
2025-11-08 17:32:18 -0600

    problem resolved

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -17,6 +17,20 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
+
+    def test_messages(self):
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here',
+            category='A category'
+        ), follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'A category' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
